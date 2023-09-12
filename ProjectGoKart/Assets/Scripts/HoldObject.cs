22cb8b40da42620b7593f97afdc26db1b09fb275
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2023-09-11 20:48:49 -0500

    tried to make ball follow cart when hit but code is broken
    When the kart collides with the ball the ball is supposed to follow the kart in a position, but it doesn't.

--- ProjectGoKart/Assets/Scripts/HoldObject.cs
+++ ProjectGoKart/Assets/Scripts/HoldObject.cs
@@ -4,67 +4,71 @@ using UnityEngine;
 
 public class HoldObject : MonoBehaviour
 {
-    [SerializeField] Transform hold;
+    //[SerializeField] Transform hold;
     private GameObject holdObj;
     private Rigidbody rigidbody;
+    public GameObject grab;
+    [SerializeField] float speed = 1.5f;
 
-    [SerializeField] private float pickupRan = 5f;
-    [SerializeField] private float pickupFor = 5f;
+    //[SerializeField] private float pickupRan = 5f;
+    //[SerializeField] private float pickupFor = 5f;
 
     private void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "Ball")
         {
-            if (holdObj == null)
-            { 
-                RaycastHit hit;
-                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRan))
-                {
-                    Pickup(hit.transform.gameObject);
-                }
+            transform.position = Vector3.MoveTowards(transform.position, grab.transform.position, speed * Time.deltaTime);
+
+            //if (holdObj == null)
+            //{ 
+            //    RaycastHit hit;
+            //    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, pickupRan))
+            //    {
+            //        Pickup(hit.transform.gameObject);
+            //    }
             
-            }
-            else
-                {
-                Drop();
-                }
-            if (holdObj != null)
-            {
-                MoveObj();
-            }
+            //}
+            //else
+            //    {
+            //    Drop();
+            //    }
+            //if (holdObj != null)
+            //{
+            //    MoveObj();
+            //}
 
         }
     }
 
-    void MoveObj()
-    {
-        if (Vector3.Distance(holdObj.transform.position, hold.position) > 0.1f)
-        {
-            Vector3 moveDirection = (hold.position - holdObj.transform.position);
-            rigidbody.AddForce(moveDirection * pickupFor);
-        }
-    }
-    void Pickup(GameObject pickObj)
-    {
-        if (pickObj.GetComponent<Rigidbody>())
-        {
-            rigidbody = pickObj.GetComponent<Rigidbody>();
-            rigidbody.useGravity = false;
-            rigidbody.drag = 10;
-            rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    //void MoveObj()
+    //{
+    //    if (Vector3.Distance(holdObj.transform.position, hold.position) > 0.1f)
+    //    {
+    //        Vector3 moveDirection = (hold.position - holdObj.transform.position);
+    //        rigidbody.AddForce(moveDirection * pickupFor);
+    //    }
+    //}
+    //void Pickup(GameObject pickObj)
+    //{
+    //    if (pickObj.GetComponent<Rigidbody>())
+    //    {
+    //        rigidbody = pickObj.GetComponent<Rigidbody>();
+    //        rigidbody.useGravity = false;
+    //        rigidbody.drag = 10;
+    //        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
 
-            rigidbody.transform.parent = hold;
-            holdObj = pickObj;
-        }
-    }
-    void Drop()
-    {
-            rigidbody.useGravity = false;
-            rigidbody.drag = 1;
-            rigidbody.constraints = RigidbodyConstraints.None;
+    //        rigidbody.transform.parent = hold;
+    //        holdObj = pickObj;
+    //    }
+    //}
+    //void Drop()
+    //{
+    //        rigidbody.useGravity = false;
+    //        rigidbody.drag = 1;
+    //        rigidbody.constraints = RigidbodyConstraints.None;
 
-            rigidbody.transform.parent = null;
-            holdObj = null;
+    //        rigidbody.transform.parent = null;
+    //        holdObj = null;
         
-    }
+    //}
 }
