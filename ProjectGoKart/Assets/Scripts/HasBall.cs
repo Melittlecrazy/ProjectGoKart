c92ee361af9913d061c6ce352f6fd194a1282da4
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2023-09-18 20:27:53 -0500

    fixed score system, it now is in order
    that order is dependent on what quadrant you start in.

--- ProjectGoKart/Assets/Scripts/HasBall.cs
+++ ProjectGoKart/Assets/Scripts/HasBall.cs
@@ -7,13 +7,16 @@ using TMPro;
 public class HasBall : MonoBehaviour
 {
     public GameObject ball;
-    public GameObject trail;
+    public GameObject trail, quadMan;
+    public MeshRenderer quad1,quad2,quad3,quad4;
     public Material quadOne,quadTwo,quadThree,quadFour;
     public int point = 0;
     public TextMeshProUGUI score;
 
     bool particle = false;
     bool hasBall = false;
+
+
 
     void Start()
     {
@@ -32,6 +35,7 @@ public class HasBall : MonoBehaviour
     {
         score.text = "Score: " + point;
        if (point == 4) score.text = "WINNER";
+        quadMan.SetActive(false);
     }
     private void OnCollisionEnter(Collision col)
     {
@@ -40,24 +44,45 @@ public class HasBall : MonoBehaviour
             trail.SetActive(true);
             hasBall = true; 
         }
-
+        
         
         //quadTwo.SetColor()
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Finish" && hasBall == true)
-        {
-            quadOne.SetColor("_Color", Color.red);
-            point = point + 1;
 
-            quadTwo.SetColor("_Color", Color.red);
-            //point = point + 1;
-            quadThree.SetColor("_Color", Color.red);
-            //point = point + 1;
-            quadFour.SetColor("_Color", Color.red);
-            //point = point + 1;
-        }
+        if (other.gameObject.name == "quad1" && hasBall == true) { QuadA(); quadOne.SetColor("_Color", Color.red); point = point + 1; quad1.enabled = true; }
+        if (other.gameObject.name == "quad2" && hasBall == true) { QuadB(); quadTwo.SetColor("_Color", Color.red); point = point + 1; quad2.enabled = true; }
+        if (other.gameObject.name == "quad3" && hasBall == true) { QuadC(); quadThree.SetColor("_Color", Color.red); point = point + 1; quad3.enabled = true; }
+        if (other.gameObject.name == "quad4" && hasBall == true) { QuadD(); quadFour.SetColor("_Color", Color.red); point = point + 1; quad4.enabled = true; }
+
     }
-    
+    void QuadA()
+    {
+        //quad1.enabled= true;
+        //quad2.enabled = true;
+        //quad3.enabled = true;
+        quad4.enabled= true;
+    }
+    void QuadB()
+    {
+        quad1.enabled= true;
+        //quad2.enabled= true;
+        //quad3.enabled= true;
+        //quad4.enabled= true;
+    }
+    void QuadC()
+    {
+        //quad1.enabled= true;
+        quad2.enabled= true;
+        //quad3.enabled= true;
+        //quad4.enabled= true;
+    }
+    void QuadD()
+    {
+        //quad1.enabled= true;
+        //quad2.enabled= true;
+        quad3.enabled= true;
+        //quad4.enabled= true;
+    }
 }
