bf6982753f2f830b0dcaebf4d8f0a265c1606c7d
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2023-10-29 14:35:36 -0500

    added materials to the Tinkerin Tom car

---
+++ ProjectGoKart/Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Debug.Log("Entered Trigger.");
+    }
+}
