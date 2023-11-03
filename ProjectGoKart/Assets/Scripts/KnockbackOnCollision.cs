e352ffb0168d580134eb7f9466cd8a0c5a5b4a0e
JCDM122300 <55261863+JCDM122300@users.noreply.github.com>
2023-11-03 15:13:06 -0500

    attempted to fix stun timer

--- ProjectGoKart/Assets/Scripts/KnockbackOnCollision.cs
+++ ProjectGoKart/Assets/Scripts/KnockbackOnCollision.cs
@@ -12,35 +12,40 @@ public class KnockbackOnCollision : MonoBehaviour
     {
         BasicKartMove moveScript = GetComponent<BasicKartMove>();
         Rigidbody rb = collision.collider.GetComponent<Rigidbody>();
+        stunTimer -= Time.deltaTime;
 
         if (rb != null)
         {
             Vector3 direction = collision.transform.position - transform.position;
             direction.y = 0f;
+            
 
             rb.AddForce(direction.normalized * knockbackStrength, ForceMode.VelocityChange);
 
             if(collision.gameObject.tag == "Player")
             {
                 isBumpedP1 = true;
-                if(stunTimer > 0f && isBumpedP1 == true)
+                
+                if (stunTimer > 0f)
                 {
-                    stunTimer -= Time.deltaTime;
+                    
                     moveScript.currentSpeed1 = 0f;
                 }
                 if (stunTimer <= 0f)
                 {
-                    moveScript.currentSpeed1 = moveScript.speed;
+                   isBumpedP1 = false;
+                  moveScript.currentSpeed1 = moveScript.speed;
+                    
                 }
                 
 
             }
             if(collision.gameObject.tag == "Enemy")
             {
-                isBumpedP2 = true;
-                if (stunTimer > 0f && isBumpedP2 == true)
+                
+                if (stunTimer > 0f)
                 {
-                    stunTimer -= Time.deltaTime;
+                    
                     moveScript.currentSpeed2 = 0f;
                 }
                 if (stunTimer <= 0f)
